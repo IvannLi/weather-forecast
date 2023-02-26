7c265998547a0309235b564f6c7e6c21e15fe308
IvannLi <ili25@my.bcit.ca>
2023-02-26 00:02:51 -0800

    grabWeather bug fix

--- pages/index.js
+++ pages/index.js
@@ -102,9 +102,9 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if (grabWeather.current === true) {
-      fetchWeather();
-    }
+    // if (grabWeather.current === true) {
+    //   fetchWeather();
+    // }
     return () => {
       grabWeather.current = true;
     }
